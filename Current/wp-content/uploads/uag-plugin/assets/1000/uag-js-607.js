59d952707d60f0ccf74745dbcad0b392232eb87c
asperbaawp <asperbaawp@gmail.com>
2024-08-03 11:53:19 -0500

    Aug 3 2.0

---
+++ Current/wp-content/uploads/uag-plugin/assets/1000/uag-js-607.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-8df6a69a .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-8df6a69a .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-8df6a69a .swiper-button-next","prevEl":".uagb-block-8df6a69a .swiper-button-prev"}}	);
+});
+
+ });
