7c848a1e6ae8b49870f58f5c8f509989aae8eb6f
asperbaawp <asperbaawp@gmail.com>
2024-08-02 19:33:41 -0500

    Aug 02 Launch 1

--- Current/wp-content/uploads/uag-plugin/assets/1000/uag-js-607.js
+++ Current/wp-content/uploads/uag-plugin/assets/1000/uag-js-607.js
@@ -1,13 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-03232925' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-48612f77' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-0456a63f' );
-});
-window.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
 	var swiper = new Swiper( ".uagb-block-8df6a69a .uagb-swiper",
 		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-8df6a69a .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-8df6a69a .swiper-button-next","prevEl":".uagb-block-8df6a69a .swiper-button-prev"}}	);
 });
